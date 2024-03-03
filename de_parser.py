2be8dc5c532a654edf5aa7ad9f1c115de33b7a45
SSQ <110625524+ssq0-0@users.noreply.github.com>
2024-03-03 14:19:42 +0300

    Update de_parser.py

--- de_parser.py
+++ de_parser.py
@@ -1,23 +1,46 @@
 import requests
-import apikey
 import aiohttp
 import logging
-logging.basicConfig(level=logging.INFO)
+import datetime
 from tg_token import api_key
 
 
+logging.basicConfig(level=logging.INFO)
+
+last_request_time = None
+cached_data = None
+
+
 async def cryptorank_requests():
-    logging.info("Making request to CryptoRank API")
-    link = f"https://api.cryptorank.io/v1/currencies?{api_key}"
-    data = requests.get(link)  # Этот вызов должен быть асинхронным, если функция асинхронная
-    logging.info(f"Response from CryptoRank API: {data.text}")
-    return data.json()  # Предполагается, что вы хотите работать с данными в формате JSON
+    global last_request_time, cached_data
+    current_time = datetime.datetime.now()
+
+    if last_request_time is None or (current_time - last_request_time) > datetime.timedelta(days=1):
+        logging.info("Making request to CryptoRank API")
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+            link = f"https://api.cryptorank.io/v1/currencies?{api_key}"
+            async with session.get(link) as response:
+                data = await response.json()
+                logging.info("Response from CryptoRank API received")
+                cached_data = data
+                last_request_time = current_time
+    else:
+        logging.info("Using cached data")
+    return cached_data
 
 
-async def coinpars(coinname):
+async def id_pars(coinname):
     data = await cryptorank_requests()
     for token in data['data']:
         if token['name'].lower() == coinname or token['symbol'].lower() == coinname:
-            print(token)
-            return token['id']
+            result = await price_pars(token['id'])
+            return f"{result}$"
 
+
+async def price_pars(id):
+    link = f"https://api.cryptorank.io/v1/currencies/{id}?{api_key}"
+    response =  requests.get(link)
+    data = response.json()
+    price_info = data['data']['values']['USD']
+    price = price_info['price']
+    return f"{price:.2f}"
