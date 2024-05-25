792ac50c91c3c6b079edbe78ee9ef42ff8c645fb
SSQ <110625524+ssq0-0@users.noreply.github.com>
2024-05-25 22:04:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-# import apikey
-# import requests
-#
-#
-# api = 'api_key=8ee1428c3dbdf56cf18e716b5b47152872f86d68223149b9403edc418d2a'
-#
-# params = {
-#     'key1': 1,
-#     'key2': api
-# }
-#
-# link = f'https://api.cryptorank.io/v1/currencies/{params["key1"]}?{api}'
-# response = requests.get(link, params=params)
-# print(link)
-# none = 'https://api.cryptorank.io/v1/currencies/?api_key=8ee1428c3dbdf56cf18e716b5b47152872f86d68223149b9403edc418d2a'
-# print(response.text)
 import asyncio
 from aiogram import Bot, Dispatcher
 from tg_token import token
